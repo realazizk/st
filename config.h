e77dad9f80ccd8eca508e1bf23f8c956bac95acb
mohamed-aziz <medazizknani@gmail.com>
2018-01-31 11:43:52 +0100

    Add light version

--- config.h
+++ config.h
@@ -83,40 +83,47 @@ char termname[] = "st-256color";
  */
 static unsigned int tabspaces = 8;
 
-/* Terminal colors (16 first used in escape sequence) */
 const char *colorname[] = {
-	"#171717",
-	"#CC0000",
-	"#4E9A06",
-	"#C4A000",
-	"#3465A4",
-	"#75507B",
-	"#06989A",
-	"#D3D7CF",
 
-	"#555753",
-	"#EF2929",
-	"#8AE234",
-	"#FCE94F",
-	"#729FCF",
-	"#AD7FA8",
-	"#34E2E2",
-	"#EEEEEC",
+  /* 8 normal colors */
+  [0] = "#002b36", /* black   */
+  [1] = "#dc322f", /* red     */
+  [2] = "#859900", /* green   */
+  [3] = "#b58900", /* yellow  */
+  [4] = "#268bd2", /* blue    */
+  [5] = "#6c71c4", /* magenta */
+  [6] = "#2aa198", /* cyan    */
+  [7] = "#93a1a1", /* white   */
 
-	[255] = 0,
+  /* 8 bright colors */
+  [8]  = "#657b83", /* black   */
+  [9]  = "#dc322f", /* red     */
+  [10] = "#859900", /* green   */
+  [11] = "#b58900", /* yellow  */
+  [12] = "#268bd2", /* blue    */
+  [13] = "#6c71c4", /* magenta */
+  [14] = "#2aa198", /* cyan    */
+  [15] = "#fdf6e3", /* white   */
 
-	"#cccccc",
+  /* special colors */
+  [256] = "#fdf6e3", /* background */
+  [257] = "#586e75", /* foreground */
 };
-
 
 /*
  * Default colors (colorname index)
- * foreground, background, cursor, reverse cursor
+ * foreground, background, cursor
  */
-unsigned int defaultfg = 7;
-unsigned int defaultbg = 0;
-unsigned int defaultcs = 256;
+unsigned int defaultfg = 257;
+unsigned int defaultbg = 256;
+unsigned int defaultcs = 257;
 unsigned int defaultrcs = 257;
+
+/*
+* Default colors (colorname index)
+* foreground, background, cursor, reverse
+* cursor
+*/
 
 /*
  * Default shape of cursor
